678df918c0bea25de63f336d577248d982ea5e20
jenny-speelmon <jspeel@uw.edu>
2026-02-08 16:13:19 -0800

    added beads to necklace sk3

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -1,6 +1,9 @@
 // Instance-mode sketch for tab 3
 registerSketch('sk3', function (p) {
   let cx, cy;
+  let beadCount = 48;
+  let ovalW = 300;
+  let ovalH = 420;
 
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
@@ -16,6 +19,21 @@ registerSketch('sk3', function (p) {
     p.stroke(160);
     p.strokeWeight(3);
     p.ellipse(cx, cy, 300, 420);
+
+    // beads
+    let a = ovalW / 2;
+    let b = ovalH / 2;
+
+    for (let i = 0; i < beadCount; i++) {
+      let angle = p.TWO_PI * (i / beadCount) - p.HALF_PI;
+
+      let x = cx + a * p.cos(angle);
+      let y = cy + b * p.sin(angle);
+
+      p.noStroke();
+      p.fill(180);
+      p.circle(x, y, 10);
+    }
   };
 
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight);
