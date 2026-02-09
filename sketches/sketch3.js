d8b901fc94b4f6043ae20b3cc3535195a63512a6
jenny-speelmon <jspeel@uw.edu>
2026-02-08 16:08:18 -0800

    drew necklace string sk3

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -1,14 +1,25 @@
 // Instance-mode sketch for tab 3
 registerSketch('sk3', function (p) {
+  let cx, cy;
+
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    cx = p.width / 2;
+    cy = p.height / 2 - 40;
   };
+
   p.draw = function () {
-    p.background(240, 200, 200);
-    p.fill(180, 60, 60);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. B', p.width / 2, p.height / 2);
+    p.background(255);
+   
+    // Draw the necklace string
+    p.noFill();
+    p.stroke(160);
+    p.strokeWeight(3);
+    p.ellipse(cx, cy, 300, 420);
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+
+  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight);
+    cx = p.width / 2;
+    cy = p.height / 2 - 40;
+  };
 });
